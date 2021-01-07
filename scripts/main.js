0b1c1e235448c36b361608eafc856ea421c8657d
MatthewBuy <mateusdesouzaamancio@outlook.com>
2021-01-07 16:49:20 -0300

    primeiro commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+function validarSenha(){
+
+    var senha = document.getElementById("inputPassword5").value;
+
+    /*if(senha.length <= 5){
+    alert("Preencha o campo senha com mínimo 6 caracteres")
+    senha.focu();
+    return false;
+}*/
+
+}
