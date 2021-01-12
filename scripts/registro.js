6035d55360090e1b3d1a0fa887cdd67e2bd5567e
MatthewBuy <mateusdesouzaamancio@outlook.com>
2021-01-12 14:33:52 -0300

    Adicionando validação visual

--- scripts/registro.js
+++ scripts/registro.js
@@ -1,4 +1,23 @@
 
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Pega todos os formulários que nós queremos aplicar estilos de validação Bootstrap personalizados.
+      var forms = document.getElementsByClassName('needs-validation');
+      // Faz um loop neles e evita o envio
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
+
 function fazerRegistro () {
     var nome = document.querySelector("#nome");
     var email = document.querySelector("#email");
