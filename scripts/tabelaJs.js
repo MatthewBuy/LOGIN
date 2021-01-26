60ee14199e64bd6653471f684b38a823fcbe8fa6
MatthewBuy <mateusdesouzaamancio@outlook.com>
2021-01-26 09:54:41 -0300

    Ultima versao

--- scripts/tabelaJs.js
+++ scripts/tabelaJs.js
@@ -1,4 +1,5 @@
   var tbody = document.getElementById("tbody");
+  var contadorHtml = document.getElementById("contador");
   var token = window.localStorage.getItem("token"); //verifico se no storage existe o token, e se esxistir a variavel recebe o valor.
   var contadorDePagina = 1;
 
@@ -61,10 +62,11 @@ function TabelaUsuarios(paginas) {
       var maximoItems = 10;
       var pagina = paginas;
     
-
       let totalPagina = Math.ceil( array.length / maximoItems);
       let contador = (  pagina * maximoItems ) - maximoItems;
       let delimitador = contador + maximoItems;
+      
+      
 
       if(pagina <= totalPagina) {
         for(let i=contador; i<delimitador; i++){
@@ -78,17 +80,25 @@ function TabelaUsuarios(paginas) {
                 ati = array[i].ativo = "Inativo"
             }    
             
-            var colunas = "<tr><td>  <button id='edit' onclick='Editar({0})' type='button'>"+"<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>"+"<path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>"+"<path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>"+"</svg>"+"</button>"+"<button id='edit' onclick='Deletar({0}, {1})' type='button'>"+"<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-x' viewBox='0 0 16 16'>"+"<path d='M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z'/>"+"<path fill-rule='evenodd' d='M12.146 5.146a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z'/>"+"</svg> </button>"+"</td><td>{5}</td><td>{2}</td><td>{3}</td><td>{4}</td></tr>";
+            var colunas = "<tr><td> <button id='edit' onclick='Editar({0})' type='button'>"+"<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>"+"<path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>"+"<path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>"+"</svg>"+"</button>"+"<button id='edit' onclick='Deletar({0}, {1})' type='button'>"+"<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-x' viewBox='0 0 16 16'>"+"<path d='M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z'/>"+"<path fill-rule='evenodd' d='M12.146 5.146a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z'/>"+"</svg> </button>"+"</td><td>{5}</td><td>{2}</td><td>{3}</td><td>{4}</td></tr>";
+
+           
 
             var colunasComValores = String.format(colunas, "\"" + usuario.public_id + "\"","\"" + usuario.nome + "\"", usuario.email, usuario.tipo, ati, usuario.nome);
 
             tbody.innerHTML += colunasComValores;
+
+           
           }
           contador++;
         }
 
-
-       
+        for(var j = 0; j < totalPagina; j++){
+          $("#contador tr").remove();
+          var linhaContador = "<li onclick='paginaUnica(1)' class='page-item'><a class='page-link' href='#'>2</a></li>"
+          contadorHtml.innerHTML += linhaContador;
+        }
+        
         
       }
   
@@ -112,15 +122,7 @@ function TabelaUsuarios(paginas) {
         tbody.innerHTML += colunasComValores;
 
         
-      }/*
-
-     /* $('.demo2').bootpag({
-        total: 23,
-        page: 3,
-        maxVisible: 10
-      }).on('page-item', function(event, resposta){
-         $(".pagination").html("Page-item " + resposta); // or some ajax content loading...
-      });*/
+      }*/
 
     }    
   });
